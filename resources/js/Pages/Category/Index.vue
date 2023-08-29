a5737ca44713a3aa1d050e66afaf892f9991ce38
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-08-29 18:06:27 +0300

    hello world

---
+++ resources/js/Pages/Category/Index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts" >
+import BaseLayout from '@/Layouts/BaseLayout.vue'
+
+defineProps<{
+    name: string,
+    luckyNumber: number
+}>()
+
+</script>
+
+
+<template>
+    <BaseLayout>
+        <h1>{{ name }}</h1>
+        <h2>{{ luckyNumber }}</h2>
+    </BaseLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -39,5 +40,8 @@ Route::middleware('auth')->group(function () {
 Route::get('/testing', function () {
     return Inertia::render("Test");
 });
+
+Route::resource('/category', CategoryController::class);
+
 
 require __DIR__ . '/auth.php';
